f14bebf40c7b0826d85306ed79a98e85901b9347
Ankur Singh <ankurks@thoughtworks.com>
2015-02-18 10:25:58 +0530

    methods names modified get bytelength to countCharacters getNoOfWords to countWords getNumberOfLines to countLines

--- WcLib.java
+++ WcLib.java
@@ -1,6 +1,6 @@
 public class WcLib{
 
-	public static int getByteLength(String string){
+	public static int countCharacters(String string){
 		return string.length();
 	}
 
@@ -8,12 +8,12 @@ public class WcLib{
 		return string.length()-string.replace(" ","").length();
 	}
 
-	public static int getNumberOfWords(String string){
+	public static int countWords(String string){
 		String trimmed=string.trim();
 		return trimmed.split("\\W+").length;
 	}
 
-	public static int getNumberOfLines(String string){
+	public static int countLines(String string){
 		return string.split("\r\n|\r|\n").length;
 	}
 
--- WcTest.java
+++ WcTest.java
@@ -4,52 +4,52 @@ import static org.junit.Assert.*;
 public class WcTest{
 
 	@Test
-	public void getByteLength_gives_10_when_string_lenght_is_10(){
+	public void countCharacters_gives_10_when_string_lenght_is_10(){
 		String string="I am rukna";
-		assertEquals(10, WcLib.getByteLength(string));
+		assertEquals(10, WcLib.countCharacters(string));
 	}
 
 
 	@Test
-	public void getByteLength_gives_24_when_string_lenght_is_24(){
+	public void countCharacters_gives_24_when_string_lenght_is_24(){
 		String string="I am swamiji STEP father";
-		assertEquals(24, WcLib.getByteLength(string));
+		assertEquals(24, WcLib.countCharacters(string));
 	}
 
 	@Test
-	public void getByteLength_gives_46_when_string_lenght_is_46(){
+	public void countCharacters_gives_46_when_string_lenght_is_46(){
 		String string="Jayanth is STEP mother and Swamiji STEP father";
-		assertEquals(46, WcLib.getByteLength(string));
+		assertEquals(46, WcLib.countCharacters(string));
 	}
 
 	@Test
-	public void getNumberOfWords_gives_3_when_3_words_are_in_string(){
+	public void countWords_gives_3_when_3_words_are_in_string(){
 		String string="I am rukna";
-		assertEquals(3, WcLib.getNumberOfWords(string));
+		assertEquals(3, WcLib.countWords(string));
 	}
 
 	@Test
-	public void getNumberOfWords_gives_3_when_3_words_are_in_string_string_having_outer_spaces(){
+	public void countWords_gives_3_when_3_words_are_in_string_string_having_outer_spaces(){
 		String string="        I am rukna       ";
-		assertEquals(3, WcLib.getNumberOfWords(string));
+		assertEquals(3, WcLib.countWords(string));
 	}
 
 	@Test
-	public void getNumberOfWords_gives_3_when_3_words_are_in_string_string_having_extra_inner_spaces(){
+	public void countWords_gives_3_when_3_words_are_in_string_string_having_extra_inner_spaces(){
 		String string="I        am     rukna";
-		assertEquals(3, WcLib.getNumberOfWords(string));
+		assertEquals(3, WcLib.countWords(string));
 	}
 
 	@Test
-	public void getNumberOfWords_gives_3_when_3_words_are_in_string_string_having_extra_inner_spaces_and_outer_spaces(){
+	public void countWords_gives_3_when_3_words_are_in_string_string_having_extra_inner_spaces_and_outer_spaces(){
 		String string="             I        am     rukna                ";
-		assertEquals(3, WcLib.getNumberOfWords(string));
+		assertEquals(3, WcLib.countWords(string));
 	}
 
 	@Test
-	public void getNumberOfWords_gives_6_when_6_words_are_in_string_string_having_new_line(){
+	public void countWords_gives_6_when_6_words_are_in_string_string_having_new_line(){
 		String string=" I am rukna\nYou are you";
-		assertEquals(6, WcLib.getNumberOfWords(string));
+		assertEquals(6, WcLib.countWords(string));
 	}
 
 	@Test
@@ -77,51 +77,50 @@ public class WcTest{
 	}
 
 	@Test
-	public void getNumberOfLines_gives_1_when_one_lines_is_in_string(){
+	public void countLines_gives_1_when_one_lines_is_in_string(){
 		String string="I am rukna";
-		assertEquals(1, WcLib.getNumberOfLines(string));
+		assertEquals(1, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_2_when_2_lines_are_in_string(){
+	public void countLines_gives_2_when_2_lines_are_in_string(){
 		String string="I am rukna\n ";
-		assertEquals(2, WcLib.getNumberOfLines(string));
+		assertEquals(2, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_3_when_3_lines_are_in_string(){
+	public void countLines_gives_3_when_3_lines_are_in_string(){
 		String string="I am rukna\nbut my name is Ankur\nConfuse ho gaye na!";
-		assertEquals(3, WcLib.getNumberOfLines(string));
+		assertEquals(3, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_2_when_2_lines_are_in_string_with_s_r(){
+	public void countLines_gives_2_when_2_lines_are_in_string_with_s_r(){
 		String string="I am rukna\rbut my name is Ankur";
-		assertEquals(2, WcLib.getNumberOfLines(string));
+		assertEquals(2, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_3_when_3_lines_are_in_string_s_r(){
+	public void countLines_gives_3_when_3_lines_are_in_string_s_r(){
 		String string="I am rukna\rbut my name is Ankur\rConfuse ho gaye na!";
-		assertEquals(3, WcLib.getNumberOfLines(string));
+		assertEquals(3, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_2_when_2_lines_are_in_string_with_s_r_s_n(){
+	public void countLines_gives_2_when_2_lines_are_in_string_with_s_r_s_n(){
 		String string="I am rukna\r\nbut my name is Ankur";
-		assertEquals(2, WcLib.getNumberOfLines(string));
+		assertEquals(2, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_3_when_3_lines_are_in_string_with_s_r_s_n(){
+	public void countLines_gives_3_when_3_lines_are_in_string_with_s_r_s_n(){
 		String string="I am rukna\r\nbut my name is Ankur\r\nConfuse ho gaye na!";
-		assertEquals(3, WcLib.getNumberOfLines(string));
+		assertEquals(3, WcLib.countLines(string));
 	}
 
 	@Test
-	public void getNumberOfLines_gives_4_when_4_lines_are_in_string_with_s_r_s_n(){
+	public void countLines_gives_4_when_4_lines_are_in_string_with_s_r_s_n(){
 		String string="I am rukna\r\nbut my name is Ankur\r\nhasi aa rahi hai\r\nmain aisa q hoon mujhe bhi ni pata";
-		assertEquals(4, WcLib.getNumberOfLines(string));
+		assertEquals(4, WcLib.countLines(string));
 	}
-
 }
